0e485d5ea1164d409d3400ac54fa45953be49823
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2019-02-28 14:10:47 -0700

    Delete IExitable.java

--- IExitable.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface IExitable {
-	
-	public void exitSubcribe(SubscribesToExitable subscriber);
-	public void exitUnSubcribe(SubscribesToExitable subscriber);
-	public void exit();
-
-}
