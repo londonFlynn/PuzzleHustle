28b499ee2dd028ea38defa49e1031c7ec31931af
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2019-02-27 17:01:59 -0700

    Add files via upload

---
+++ Puzzle.java
@@ -0,0 +1,85 @@
+package models;
+
+import java.io.Serializable;
+
+import enums.PuzzleType;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import views.Display;
+
+public abstract class Puzzle implements Serializable {
+
+	protected BorderPane puzzlePane = new BorderPane();
+	protected Scene scene;
+	private static final long serialVersionUID = 1L;
+	private double timeSpent = 0;
+	private float score = 0;
+	private boolean solved = false;
+	private String filePath;
+	public final PuzzleType PUZZLE_TYPE;
+	private User user;
+	private Display display;
+	
+	public Puzzle(String filePath, PuzzleType PUZZLE_TYPE, User user) {
+		setFilePath(filePath);
+		this.PUZZLE_TYPE = PUZZLE_TYPE;
+		setUser(user);
+	}
+	public void Save() {
+		
+	}
+	public void delete() {
+		
+	}
+	public Scene getScene() {
+		this.scene = display.getScene();
+		this.puzzlePane = display.getMainView();
+		return scene;
+	}
+	
+	public void exit() {
+		
+	}
+	public abstract void showInstructions();
+	
+	public double getTimeSpent() {
+		return timeSpent;
+	}
+	public void setTimeSpent(double timeSpent) {
+		this.timeSpent = timeSpent;
+	}
+	public float getScore() {
+		return score;
+	}
+	public void setScore(float score) {
+		this.score = score;
+	}
+	public boolean isSolved() {
+		return solved;
+	}
+	public void setSolved(boolean solved) {
+		this.solved = solved;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public PuzzleType getPUZZLE_TYPE() {
+		return PUZZLE_TYPE;
+	}
+	protected abstract void setupPuzzlePane();
+	
+	
+
+}
