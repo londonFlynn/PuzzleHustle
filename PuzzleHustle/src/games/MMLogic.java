08e5cdbb4effecbc6c8c1423bbcc4d23f5747bee
WinnieQueen <47342826+WinnieQueen@users.noreply.github.com>
2019-03-01 16:14:18 -0700

    Updated MasterMind logic to near-completion

--- PuzzleHustle/src/games/MMLogic.java
+++ PuzzleHustle/src/games/MMLogic.java
@@ -10,6 +10,16 @@ public class MMLogic {
 	private Color[] puzzle = new Color[colors.length];
 	private int rightSpots = 0;
 	private int rightColors = 0;
+	private int guessesLeft = 20;
+	private boolean canCheck;
+
+	public Color[] getAnswer() {
+		Color[] tempArr = new Color[puzzle.length];
+		for (int i = 0; i < puzzle.length; i++) {
+			tempArr[i] = puzzle[i];
+		}
+		return tempArr;
+	}
 
 	public void createPuzzle() {
 		Random rng = new Random();
@@ -24,36 +34,51 @@ public class MMLogic {
 		for (int i = 0; i < 6; i++) {
 			System.out.println(puzzle[i]);
 		}
-		
+
 	}
 
-	public boolean checkGuess(Rectangle[] squares) {
-		boolean canCheck = true;
-		for (Rectangle r : squares) {
-			canCheck = r.getFill() != Color.BLACK;
-			if (!canCheck) {
-				return false;
+	public void checkGuess(Rectangle[] squares) {
+		checkIfFullGuess(squares);
+		if (canCheck) {
+			rightColors = 0;
+			rightSpots = 0;
+			boolean[] flags = new boolean[squares.length];
+			for (int i = 0; i < squares.length; i++) {
+				flags[i] = squares[i].getFill() == puzzle[i];
 			}
-		}
-		rightColors = 0;
-		rightSpots = 0;
-		boolean[] flags = new boolean[squares.length];
-		for (int i = 0; i < squares.length; i++) {
-			flags[i] = squares[i].getFill() == puzzle[i];
-		}
-		for (int i = 0; i < squares.length; i++) {
-			if (squares[i].getFill() == puzzle[i]) {
-				rightSpots++;
-			} else {
-				for (int j = 0; j < squares.length; j++) {
-					if (!flags[j] && squares[i].getFill() == puzzle[j]) {
-						rightColors++;
-						break;
+			for (int i = 0; i < squares.length; i++) {
+				if (squares[i].getFill() == puzzle[i]) {
+					rightSpots++;
+				} else {
+					for (int j = 0; j < squares.length; j++) {
+						if (!flags[j] && squares[i].getFill() == puzzle[j]) {
+							rightColors++;
+							break;
+						}
 					}
 				}
 			}
+			guessesLeft--;
 		}
-		return canCheck;
+	}
+
+	private void checkIfFullGuess(Rectangle[] squares) {
+		canCheck = true;
+		for (Rectangle r : squares) {
+			canCheck = r.getFill() != Color.BLACK;
+			if (!canCheck) {
+				break;
+			}
+		}
+	}
+
+	public boolean checkIfWon(Rectangle[] squares) {
+		for (int i = 0; i < squares.length; i++) {
+			if (squares[i].getFill() != puzzle[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public int getRightSpots() {
@@ -62,5 +87,17 @@ public class MMLogic {
 
 	public int getRightColors() {
 		return rightColors;
+	}
+
+	public int getGuessesLeft() {
+		return guessesLeft;
+	}
+
+	public void setNumOfTries(int numOfTries) {
+		this.guessesLeft = numOfTries;
+	}
+
+	public boolean canCheck() {
+		return canCheck;
 	}
 }
