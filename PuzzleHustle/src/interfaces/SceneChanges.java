89119aa997861ad98a0526feccdbc1d40778016d
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2019-03-01 17:16:54 -0700

    Scene Change Interfaces

---
+++ PuzzleHustle/src/interfaces/SceneChanges.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface SceneChanges {
+	
+	public abstract void sceneChange();
+	public abstract void subscribeToSceneChange(SubscribesToSceneChange subscriber);
+	
+}
---
+++ PuzzleHustle/src/interfaces/SubscribesToSceneChange.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+import javafx.scene.Scene;
+
+public interface SubscribesToSceneChange {
+	
+	public void sceneChanged(Scene scene);
+
+}
