f9c91dd267ae47b3e17692da5c4e8b3e95dedee3
londonFlynn <47341087+londonFlynn@users.noreply.github.com>
2019-03-03 12:18:00 -0700

    main done

--- PuzzleHustle/src/application/Main.java
+++ PuzzleHustle/src/application/Main.java
@@ -1,20 +1,39 @@
 package application;
 	
+import controllers.PuzzleHub;
+import interfaces.SubscribesToExitable;
+import interfaces.SubscribesToSceneChange;
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
-public class Main extends Application {
+public class Main extends Application{
+	
+	private PuzzleHub hub = new PuzzleHub();
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			primaryStage.setScene(hub.getScene());
 			primaryStage.show();
+			new SubscribesToExitable() {
+				@Override
+				public void menuExited() {
+					primaryStage.close();
+				}
+				public void subscribe() {
+					hub.exitSubcribe(this);
+				}
+			}.subscribe();
+			new SubscribesToSceneChange() {
+				@Override
+				public void sceneChanged(Scene scene) {
+					primaryStage.setScene(scene);
+				}
+				public void subscribe() {
+					hub.subscribeToSceneChange(this);
+				}
+			}.subscribe();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
