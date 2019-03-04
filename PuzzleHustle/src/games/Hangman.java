37ea351e1589cf4b36ba9bc2feeec23195863b6b
WinnieQueen <47342826+WinnieQueen@users.noreply.github.com>
2019-03-04 03:24:48 -0700

    Updated Hangman.

--- PuzzleHustle/src/games/Hangman.java
+++ PuzzleHustle/src/games/Hangman.java
@@ -1,257 +1,169 @@
 package games;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.Random;
-
 import enums.PuzzleType;
 import interfaces.ISolveable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import models.Puzzle;
 import models.User;
 
+@SuppressWarnings("serial")
 public class Hangman extends Puzzle implements ISolveable {
 
-	public Hangman(String filePath, PuzzleType PUZZLE_TYPE, User user) {
-		super(filePath, PUZZLE_TYPE, user);
-		// TODO Auto-generated constructor stub
+	private HMLogic gameLogic = new HMLogic();
+	private Pane hangman = null;
+	private Text stats;
+	private Button giveUp = new Button("Give Up");
+	private boolean isDone = false;
+	private int gameMode = 0;
+	private TextField guessBox;
+
+	public Hangman(String filePath, User user) {
+		super(filePath, PuzzleType.HANGMAN, user);
 	}
 
-	private static char c;
-	private static char[] wordChars;
-	private static char[] correctGuesses;
-	private static char[] correctGuesses2;
-	private static char[] wrongGuesses;
-	private static boolean valid = false;
-	private String phrase;
-	private static char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-			'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-	private static String[] words = null;
-
-	public static void createDictionary() {
-		String text = "";
-		try {
-			FileReader fr = new FileReader("words2.txt");
-			BufferedReader reader = new BufferedReader(fr);
-			String line = reader.readLine();
-			while (line != null) {
-				if (line.indexOf("'") == -1) {
-					text += line + ",";
-				}
-				line = reader.readLine();
-			}
-			reader.close();
-		} catch (Exception e) {
-			System.out.println("There was a problem reading the file.\n" + e.getMessage());
-		}
-		words = text.split(",");
-	}
-
-	private String getRandomThing() {
-		Random rng = new Random();
-		int index = rng.nextInt(words.length);
-		return words[index];
-	}
-
-	private void game() {
-
-		System.out.println(
-				"Hi! Welcome to Hangman. Would you like to play one player or two player?\nFor one player, a random word will be selected for you to guess!\nFor two player, player1 will insert a word or phrase (up to 8 words) and player2 will try to guess it.");
-//		int gameMode = ConsoleIO.promptForMenuSelection(new String[] { "One Player", "Two Player" }, false, false);
-		int gameMode = 0;
-		do {
-			do {
-				if (gameMode == 2) {
-					// TODO add the multiplayer option
-//					phrase = ConsoleIO.promptForInput(
-//							"What phrase would you like to use? At least 3 characters, and no more than 8 words.",
-//							false).trim();
-				} else {
-					phrase = getRandomThing();
-				}
-				int amountOfSpaces = 0;
-				for (int i = 0; i < phrase.length(); i++) {
-					if (phrase.charAt(i) == ' ') {
-						amountOfSpaces++;
-					}
-				}
-
-			} while (!valid);
-
-			wordChars = new char[phrase.length()];
-			correctGuesses = new char[phrase.length()];
-			correctGuesses2 = new char[phrase.length()];
-			wrongGuesses = new char[6];
-
-			for (int i = 0; i < correctGuesses.length; i++) {
-				correctGuesses[i] = '_';
-			}
-			for (int i = 0; i < correctGuesses.length; i++) {
-				correctGuesses2[i] = ' ';
-			}
-
-			for (int i = 0; i < phrase.length(); i++) {
-				wordChars[i] = phrase.charAt(i);
-				c = wordChars[i];
-				if (!arrContainsLetter(alphabet)) {
-					correctGuesses[i] = wordChars[i];
-				}
-			}
-			for (int i = 0; i < correctGuesses.length; i++) {
-				c = '_';
-				valid = arrContainsLetter(correctGuesses) && correctGuesses.length >= 3;
-			}
-			if (!valid) {
-				// TODO get rid of this, but still check validity.... somehow
-//				System.out.println("Sorry, that's invalid!");
-			}
-		} while (!valid);
-
-		printGuesses();
-
-		for (int i = 0; i < wordChars.length + 6; i++) {
-			if (wrongGuesses[5] == 0) {
-
-				// TODO use the gui to refactor this whole thing.
-				int input = 0;
-				if (input == 2) {
-
-					do {
-						// TODO refactor to take from GUI textbox
-//						c = ConsoleIO.promptForChar("\nPlace a guess", 'A', 'z');
-						c = 'A';
-						valid = arrContainsLetter(alphabet);
-						if (!valid) {
-							System.out.println("You have to guess a letter!");
-						}
-					} while (!valid);
-
-					if (((arrContainsLetter(correctGuesses) || (arrContainsLetter(wrongGuesses))))) {
-						printGuesses();
-						System.out.println("\nYou already guessed that!");
-						i -= 1;
-					} else if (arrContainsLetter(wordChars)) {
-						printGuesses();
-						System.out.println("\nYou guessed one right!\n");
-						i -= 1;
-						if (checkIfWon()) {
-							// TODO idk, tbh
-//							hangingMan();
-//							System.out.println("\n\nYou won!\n");
-//							i = wordChars.length + 6;
-//							playAgain();
-						}
-					} else {
-						wrongGuesses[i] = c;
-						printGuesses();
-						System.out.println("\nYour guess was incorrect. ");
-					}
-				} else if (input == 1) {
-					// TODO ew
-//					if (input2.equalsIgnoreCase(phrase)) {
-//						System.out.println("You win!");
-//						i = wordChars.length + 6;
-//					}
-				}
-			}
+	private void resetStage(boolean isFirstTime) {
+		hangman.getChildren().clear();
+		if (isFirstTime) {
+			Label menuMessage = new Label();
+			Label menuMessage2 = new Label();
+			menuMessage.setText("How many players do you want?");
+			menuMessage.setId("message");
+			menuMessage2.setText("Note: 2 players will not count towards your stats");
+			menuMessage2.setId("message2");
+			hangman.getChildren().addAll(menuMessage, menuMessage2);
+			menuMessage.setTranslateX(120);
+			menuMessage.setTranslateY(50);
+			menuMessage2.setTranslateX(267.5);
+			menuMessage2.setTranslateY(150);
+			drawStarterButtons();
+		} else {
+			drawGiveUpButton();
+			startGame();
 		}
 	}
 
-	private static boolean arrContainsLetter(char[] array) {
-		boolean containsLetter = false;
-		for (int i = 0; i < array.length; i++) {
-			containsLetter = array[i] == c;
-			if (array == wordChars) {
-				if (containsLetter) {
-					correctGuesses[i] = c;
-				} else {
-					containsLetter = ((array[i] + 32) == c);
-					if (containsLetter) {
-						correctGuesses[i] = array[i];
-					} else {
-						containsLetter = ((array[i] - 32) == c);
-						if (containsLetter) {
-							correctGuesses[i] = array[i];
-						}
+	Text puzzle;
+
+	private void createPuzzle() {
+		gameLogic.game();
+		puzzle = new Text(gameLogic.printGuesses());
+		guessBox = new TextField();
+		guessBox.lengthProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if (guessBox.getText().length() >= 1) {
+					if (guessBox.getText().length() > 1) {
+						guessBox.setText(guessBox.getText().substring(0, 1));
 					}
-				}
-			} else if (array == alphabet) {
-				if (containsLetter) {
-					return true;
-				} else {
-					containsLetter = ((array[i] + 32) == c);
-					if (containsLetter) {
-						return true;
-					} else {
-						containsLetter = ((array[i] - 32) == c);
-						if (containsLetter) {
-							return true;
-						}
+					char c = guessBox.getText().charAt(0);
+					if ((c < 65 || c > 122) || (c >= 91 && c <= 96)) {
+						guessBox.setText("");
 					}
 				}
 			}
-		}
-		for (int i = 0; i < array.length; i++) {
-			containsLetter = array[i] == c;
-			if (array == wordChars) {
-				if (containsLetter) {
-					correctGuesses2[i] = c;
-					return true;
-				} else {
-					containsLetter = ((array[i] + 32) == c);
-					if (containsLetter) {
-						correctGuesses2[i] = array[i];
-						return true;
-					} else {
-						containsLetter = ((array[i] - 32) == c);
-						if (containsLetter) {
-							correctGuesses2[i] = array[i];
-							return true;
-						}
-					}
-				}
-			} else {
-				if (containsLetter) {
-					return true;
-				} else {
-					containsLetter = ((array[i] + 32) == c);
-					if (containsLetter) {
-						return true;
-					} else {
-						containsLetter = ((array[i] - 32) == c);
-						if (containsLetter) {
-							return true;
-						}
-					}
+
+		});
+		guessBox.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				if (guessBox.getText().length() >= 1) {
+					gameLogic.setGuess(guessBox.getText().charAt(0));
+					guessBox.setText("");
+					checkGuess();
 				}
 			}
-		}
-		return containsLetter;
+		});
+
+		hangman.getChildren().addAll(puzzle, guessBox);
+		guessBox.setTranslateY(400);
+		guessBox.setTranslateX(20);
+		guessBox.setMaxWidth(30);
+		puzzle.setId("puzzle");
+		puzzle.setTranslateY(420);
+		puzzle.setTranslateX(10);
 	}
 
-	private static void printGuesses() {
-
-		// TODO refactor this with the GUI
-
-		System.out.print("\nCorrect guesses so far: ");
-		for (int a = 0; a < correctGuesses.length; a++) {
-			System.out.print(correctGuesses[a] + " ");
+	protected void checkGuess() {
+		if (gameLogic.checkGuess()) {
+			puzzle.setText(gameLogic.printGuesses());
+		} else {
+			hangman.getChildren().remove(guessBox);
+			puzzle.setText("The answer was " + "\" " + gameLogic.showAnswer() + "\"" + "\nYou guessed: "
+					+ gameLogic.getAllGuesses().trim());
+			puzzle.setTranslateY(470);
 		}
-		System.out.print("\nWrong guesses so far: ");
-		for (int i = 0; i < wrongGuesses.length; i++) {
-			System.out.print(wrongGuesses[i] + " ");
-		}
-		System.out.println("\n");
 	}
 
-	private boolean checkIfWon() {
-		boolean won = false;
-		for (int i = 0; i < correctGuesses.length; i++) {
-			won = correctGuesses[i] == wordChars[i];
-			if (!won) {
-				return false;
+	private void startGame() {
+		Image gallowPic = new Image("File:Picture1.png");
+		ImageView gallows = new ImageView(gallowPic);
+		hangman.getChildren().add(gallows);
+
+		gallows.setFitHeight(500);
+		gallows.setPreserveRatio(true);
+		gallows.setTranslateY(-50);
+		gallows.setTranslateX(250);
+		createPuzzle();
+	}
+
+	private void drawStarterButtons() {
+		Button button1 = new Button("1");
+		Button button2 = new Button("2");
+		button1.setId("starterButton");
+		button2.setId("starterButton");
+		button1.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				gameMode = 1;
+				resetStage(false);
 			}
-		}
-		return won;
+		});
+		button2.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				gameMode = 2;
+				resetStage(false);
+			}
+		});
+		hangman.getChildren().addAll(button1, button2);
+		button1.setTranslateX(290);
+		button1.setTranslateY(200);
+		button2.setTranslateX(590);
+		button2.setTranslateY(200);
+	}
+
+	private void drawGiveUpButton() {
+		display.getLeftSidebar().getChildren().remove(giveUp);
+		giveUp.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				showSolution();
+				isDone = true;
+			}
+		});
+		display.getLeftSidebar().getChildren().add(giveUp);
+	}
+
+	private void drawStats() {
+
+	}
+
+	@Override
+	public void showSolution() {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
@@ -262,19 +174,25 @@ public class Hangman extends Puzzle implements ISolveable {
 
 	@Override
 	public void hideInstructions() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void setupPuzzlePane() {
-		// TODO Auto-generated method stub
-
+		hangman = new Pane();
+		drawStats();
+		resetStage(true);
+		puzzlePane.getChildren().add(hangman);
+		puzzlePane.getStylesheets().add("views/HM.css");
 	}
 
 	@Override
-	public void showSolution() {
-		// TODO Auto-generated method stub
-
+	public Scene getScene() {
+		if (scene == null) {
+			super.getScene();
+			setupPuzzlePane();
+		}
+		return scene;
 	}
+
 }
