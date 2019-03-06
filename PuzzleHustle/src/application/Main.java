05a8ab3a3b71d3b78f0ba248c4714f70fdc1380e
unlikeplatypus <43973727+unlikeplatypus@users.noreply.github.com>
2019-03-05 17:23:37 -0700

    Update Main.java

--- PuzzleHustle/src/application/Main.java
+++ PuzzleHustle/src/application/Main.java
@@ -1,16 +1,23 @@
 package application;
-	
+
+import controllers.MusicManager;
 import controllers.PuzzleHub;
 import interfaces.SubscribesToExitable;
 import interfaces.SubscribesToSceneChange;
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.EventTarget;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TreeCell;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+public class Main extends Application {
 
-public class Main extends Application{
-	
 	private PuzzleHub hub = new PuzzleHub();
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -21,6 +28,7 @@ public class Main extends Application{
 				public void menuExited() {
 					primaryStage.close();
 				}
+
 				public void subscribe() {
 					hub.exitSubcribe(this);
 				}
@@ -30,20 +38,41 @@ public class Main extends Application{
 				public void sceneChanged(Scene scene) {
 					primaryStage.setScene(scene);
 				}
+
 				public void subscribe() {
 					hub.subscribeToSceneChange(this);
 				}
 			}.subscribe();
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
 		primaryStage.setMinWidth(primaryStage.getWidth());
 		primaryStage.setMinHeight(primaryStage.getHeight());
 		primaryStage.setMaxWidth(primaryStage.getWidth());
 		primaryStage.setMaxHeight(primaryStage.getHeight());
+		primaryStage.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				EventTarget target = mouseEvent.getTarget();
+				System.out.println(target);
+				if (target instanceof Node) {
+					Node targetNode = (Node) target;
+
+					while (targetNode != null && !(targetNode instanceof Button)) {
+						targetNode = targetNode.getParent();
+					}
+					if (target instanceof Button || targetNode instanceof Button) {
+						MusicManager.playSquish();
+					}
+				}
+			}
+		});
 	}
-	
+
 	public static void main(String[] args) {
+		MusicManager.startMusic();
 		launch(args);
 	}
 }
